eff4bf83584c3c8eedd1430960979f5019b3f243
venish-vk7 <venishvk7@gmail.com>
2017-11-15 23:17:19 +0530

    Express added

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
-
+app.get('/page1',function(req,res){
+  res.sendFile(__dirname+'/pages/page1.html');
+})
+app.get('/about/:name',function(req,res){
+  res.send(req.params.name)
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
